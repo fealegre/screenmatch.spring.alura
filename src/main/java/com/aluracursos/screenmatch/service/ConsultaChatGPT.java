6d2fdc42ac244551f1d8965834213d63efc1fe49
fealegre <fealegre@hotmail.com>
2024-06-12 19:43:22 -0300

    Proyecto de consumo de API series. Generación y carga de base de datos. Uso de consultas derivadas con JPA.

--- src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
@@ -5,7 +5,7 @@ import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultaChatGPT {
     public static String obtenerTraduccion(String texto) {
-        OpenAiService service = new OpenAiService("sk-proj-DTIRBsHLPy9BsU8xCiidT3BlbkFJWrTWMBrGOExVVmpvowRw");
+        OpenAiService service = new OpenAiService();
 
         CompletionRequest requisicion = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
