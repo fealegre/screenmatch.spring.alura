cfd315f7313a98ef11542028eda2e34fc769e10c
fealegre <fealegre@hotmail.com>
2024-06-12 19:57:24 -0300

    .

--- src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
@@ -5,7 +5,7 @@ import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultaChatGPT {
     public static String obtenerTraduccion(String texto) {
-        OpenAiService service = new OpenAiService();
+        OpenAiService service = new OpenAiService("${OPENAI_TOKEN}");
 
         CompletionRequest requisicion = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
